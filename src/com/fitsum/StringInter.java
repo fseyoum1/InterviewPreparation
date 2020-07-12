837b4fd95278ba6fd9a779ee2a9b201c3f4ab2c3
fitsum <fitsum.s11@gmail.com>
2020-07-12 14:50:50 -0400

    palindrome

---
+++ src/com/fitsum/StringInter.java
@@ -0,0 +1,68 @@
+package com.fitsum;
+
+import java.util.List;
+
+public class StringInter {
+
+    public static void main(String[] args) {
+
+        String str = "ARADHAYA'S BRILLIANCE CENTER";
+        countVowels(str);
+
+    }
+
+    public static void countVowels(String str) {
+
+        char[] arr = str.toCharArray();
+
+
+        int countVowels = 0;
+        int countConsenante = 0;
+        int countNonletter = 0;
+
+        List<Character> elements = new java.util.ArrayList<Character>();
+        elements.add('A');
+        elements.add('O');
+        elements.add('U');
+        elements.add('I');
+        elements.add('E');
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= 'A' && arr[i] <= 'Z') {
+                if (elements.contains(arr[i])) {
+                    countVowels++;
+                } else {
+                    countConsenante++;
+                }
+
+            } else {
+                countNonletter++;
+            }
+        }
+
+        System.out.println("vowels " + countVowels);
+        System.out.println("consenante " + countConsenante);
+        System.out.println("non-letter " + countNonletter);
+
+
+    }
+
+    /*checks if the string contains numbers
+
+     */
+    public static void stringInteger(String str) {
+
+        int i = 0;
+        int len = str.length();
+        while (i != len) {
+            if(str.charAt(i) >= 0 && str.charAt(i) <= 9) {
+                i++;
+            } else {
+                System.out.println("Not a number");
+            }
+            if(i == len) {
+                System.out.println("All Numbers");
+            }
+        }
+
+    }
+}
