3edb966b970613dd2011df1038b793d416b851b2
fitsum <fitsum.s11@gmail.com>
2020-07-20 14:19:06 -0400

    Test

--- src/com/run/Run.java
+++ src/com/run/Run.java
@@ -4,7 +4,9 @@ public class Run {
     static int k;
     public static void main(String[] args) {
 
-        System.out.println(fib(3));
+        System.out.println(fib(10));
+        String ch = "Hello";
+        System.out.println(isUsingUniqueChars(ch));
 
     }
 
@@ -12,14 +14,35 @@ public class Run {
 
 
         if (n <= 0) {
-            System.out.println(k++);
+           // System.out.println(k++);
             return 0;
         } else if (n == 1) {
-            System.out.println(k++);
+           // System.out.println(k++);
             return 1;
+        } else {
+            return fib(n - 1) + fib(n - 2);
+
         }
-        return fib(n - 1) + fib(n - 2);
+
 
     }
 
+    public static boolean isUsingUniqueChars(String str) {
+
+        for(int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            for (int j = 0; j < str.length(); j++) {
+
+                if (ch == str.charAt(j)) {
+                    return false;
+
+                }
+                
+            }
+
+        }
+
+        return true;
+    }
 }
