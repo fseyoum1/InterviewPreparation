9bb1427912464337f22ea1fd3374e95f54a71608
fitsum <fitsum.s11@gmail.com>
2020-07-19 22:27:32 -0400

    Test

--- src/com/run/Run.java
+++ src/com/run/Run.java
@@ -1,4 +1,25 @@
 package com.run;
 
 public class Run {
+    static int k;
+    public static void main(String[] args) {
+
+        System.out.println(fib(3));
+
+    }
+
+    static int fib(int n) {
+
+
+        if (n <= 0) {
+            System.out.println(k++);
+            return 0;
+        } else if (n == 1) {
+            System.out.println(k++);
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
+
+    }
+
 }
